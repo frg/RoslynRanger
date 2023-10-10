83858f3f934b36c1b52eb9e25b281f3837833ece
Jean Farrugia <jeanfrg@gmail.com>
2023-10-10 22:41:29 +0200

    feat: added sample example

---
+++ samples/RoslynRanger.Sample/Examples.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable ReturnValueOfPureMethodIsNotUsed
+
+using System;
+
+namespace RoslynRanger.Sample;
+
+public class Examples
+{
+    public static void MathRoundMidpointExample()
+    {
+        Math.Round(1.2345); // Should trigger analyzer
+        Math.Round(1.2345, 4); // Should trigger analyzer
+
+        Math.Round(1.2345, MidpointRounding.AwayFromZero);
+        Math.Round(1.2345, MidpointRounding.ToEven);
+        Math.Round(1.2345, MidpointRounding.ToZero);
+        Math.Round(1.2345, MidpointRounding.ToNegativeInfinity);
+        Math.Round(1.2345, MidpointRounding.ToPositiveInfinity);
+    }
+}
